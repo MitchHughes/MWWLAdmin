5f540d5134d0da7fa92a310f7bd7b06cdfafa01d
Mitch <mitchell@hlhfamily.net>
2016-09-29 20:26:06 -0700

    Add Events table migration

---
+++ MWWLAdmin/MWWLAdmin.Web/Modules/Common/Migrations/DefaultDB/MwwlDB_20160929_201000_Events.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentMigrator;
+
+namespace MWWLAdmin.Migrations.DefaultDB
+{
+	[Migration(20160929201000)]
+	public class MwwlDB_20160929_201000_Events : Migration
+	{
+		public override void Up()
+		{
+			Create.Table("Events").InSchema("dbo")
+				.WithColumn("EventId").AsInt32()
+					.Identity().PrimaryKey().NotNullable()
+				.WithColumn("EventName").AsString(200).NotNullable()
+				.WithColumn("EventLocation").AsString(200).Nullable()
+				.WithColumn("EventLink").AsString(200).Nullable()
+				.WithColumn("EventStartDate").AsDateTime().Nullable()
+				.WithColumn("EventEndDate").AsDateTime().Nullable()
+				.WithColumn("EventActive").AsBoolean().NotNullable().WithDefaultValue(1)
+				;
+		}
+
+		public override void Down()
+		{
+			Delete.Table("Events");
+		}
+	}
+}
